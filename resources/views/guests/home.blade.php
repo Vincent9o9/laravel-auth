067fffd03ae28cccbf587f837ebb27e716d925f6
Vincent9o9 <66362554+Vincent9o9@users.noreply.github.com>
2020-10-21 18:51:27 +0200

    step one

---
+++ resources/views/guests/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('content')
+<div class="display-4 p-5 text-center">
+    Benvenuto nel mio fantastico blog
+</div>
+@guest
+    <p class="lead text-center">Guest</p>
+@else
+    <p class="lead">Il tuo nome è: {{ Auth::user()->name }}</p>
+@endguest
+@endsection
+{{-- C5.yZMLJe.myV$! --}}
